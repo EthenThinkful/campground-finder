81266921def5ccfebe1a9bedcc939873ef157da1
Ethen Roth <Ethenfm@gmail.com>
2023-08-18 23:02:34 -0600

    filter through recreation.gov database json of campgrounds to retrieve campground names to be used as suggestions in the dropdown of the form

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 import SimpleForm from "@/components/SimpleForm";
 
+
 const Home: React.FC = () => {
   const handleSubmit = (inputValue: string) => {
     // Process the form data here, e.g., send it to an API
     console.log("Submitted:", inputValue);
   };
   return <SimpleForm onSubmit={handleSubmit} />;
+  // return <GetNames />;
+
 };
 export default Home;
