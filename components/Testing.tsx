c4dc72dd00487c6e4628be6ec5204488ba732669
EthenThinkful <ethenfm@gmail.com>
2023-08-20 01:18:48 -0600

    successfully can render information from the api after conversions

--- components/Testing.tsx
+++ components/Testing.tsx
@@ -1,74 +1,82 @@
 import React, { useEffect, useState } from "react";
 import { parseString } from "xml2js";
 import axios from "axios";
-import Cors from 'cors';
 
 const Testing: React.FC = () => {
-  interface ApiResponse {
-    // Define the structure of the response data here
-  }
+  const [apiData, setApiData] = useState<string>('');
+  const [convertedApiData, setConvertedApiData] = useState<Array<any>>([]);
 
-  async function fetchApiData(): Promise<ApiResponse> {
+  async function fetchApiData(): Promise<String> {
     try {
       const response = await axios.get(
         "https://api.amp.active.com/camping/campgrounds?pstate=CO&api_key=scg63dkm2pf5smp8umfke2gj"
       );
-      console.log(response.data);
-      return response.data as ApiResponse;
+      // console.log(response.data);
+      return response.data;
     } catch (error) {
       throw error;
     }
   }
 
-  fetchApiData();
+  useEffect(() => {
+    async function fetchDataApi() {
+      try {
+        const data = await fetchApiData();
+        setApiData(data);
+        // console.log(data);
+        // console.log(apiData) doesn't give a response
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+    fetchDataApi();
+  }, []);
 
-//   function convertXmlToJson(xml: string): Promise<unknown> {
-//     return new Promise((resolve, reject) => {
-//       parseString(xml, (err, result) => {
-//         if (err) {
-//           reject(err);
-//         } else {
-//           resolve(result);
-//         }
-//       });
-//     });
-//   }
+  async function convertXmlToJson(xml: string): Promise<any> {
+    return new Promise((resolve, reject) => {
+      parseString(xml, (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          // console.log('http://www.reserveamerica.com' + result.resultset.result[0].$.faciltyPhoto); // grabs a specific value from a campground
+          // console.log(result);
+          setConvertedApiData(result.resultset.result);
+          resolve(result);
+        }
+      });
+    });
+  }
 
-//   interface ApiData {
-//     // Define the structure of the API response data here
-//   }
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        if (apiData) {
+          const result = await convertXmlToJson(apiData);
+          // console.log(convertedApiData);
+        }
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    }
 
-//   const [apiData, setApiData] = useState<ApiData | null>(null);
-
-//   useEffect(() => {
-//     async function fetchData() {
-//       try {
-//         const data = await fetchApiData();
-//         setApiData(data);
-//       } catch (error) {
-//         console.error("Error fetching data:", error);
-//       }
-//     }
-
-//     fetchData();
-//   }, []);
-
-//   useEffect(() => {
-//     async function fetchData() {
-//       try {
-//         const result = await convertXmlToJson(apiData);
-//         console.log(result);
-//       } catch (error) {
-//         console.error("Error:", error);
-//       }
-//     }
-
-//     fetchData();
-//   }, []);
+    fetchData();
+  }, [apiData]); // Only call this effect when apiData changes
 
   return (
     <div className="App">
-      <h1>XML to JSON Conversion</h1>
+      <h1>Campsite Photos</h1>
+      <div className="image-container">
+        {convertedApiData?.map((campground, index) => (
+          <div key={index} className="campground-images">
+            {campground.$.faciltyPhoto && (
+              <img
+                src={'http://www.reserveamerica.com' + campground.$.faciltyPhoto}
+                alt={`Campground ${index + 1}`}
+              />
+            )}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
